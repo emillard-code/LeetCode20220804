dc0ccb288765e9311fd61952de6470722d729b38
Emerson Millard <emillard.code@gmail.com>
2022-08-16 12:00:00 -0700

    Modified attempt class.

--- src/main/java/com/project/attempt/LeetCodeApp.java
+++ src/main/java/com/project/attempt/LeetCodeApp.java
@@ -1,4 +1,15 @@
 package com.project.attempt;
 
 public class LeetCodeApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int method(int p, int q) {
+
+        return 0;
+
+    }
+
 }
