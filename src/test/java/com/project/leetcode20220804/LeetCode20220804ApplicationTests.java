97c8bbfbe4d35d1b57ea9e01ce9cd34cb7cd5242
Emerson Millard <emillard.code@gmail.com>
2022-09-30 16:58:33 -0700

    Initial commit

---
+++ src/test/java/com/project/leetcode20220804/LeetCode20220804ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.leetcode20220804;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetCode20220804ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
